cb7136d3d4b5d59fbd0885842ead36968a809223
Nathrak <bob@bernelius.com>
2025-09-26 11:45:49 +0200

    slightly reduced danger

--- .config/zsh/dotfiles-symlink-watcher.sh
+++ .config/zsh/dotfiles-symlink-watcher.sh
@@ -3,14 +3,16 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTFILES_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
 
+
+
 #never again (hopefully)
 if [[ ! -d "$DOTFILES_DIR/.git" ]]; then
-    echo "DOTFILES_DIR looks wrong: $DOTFILES_DIR"
-    exit 1
+  echo "DOTFILES_DIR looks wrong: $DOTFILES_DIR"
+  exit 1
 fi
 
 skip_files=(
-  "$DOTFILES_DIR/README.md" 
+  "$DOTFILES_DIR/README.md"
   "$DOTFILES_DIR/.gitignore"
   "$DOTFILES_DIR/install.sh"
   "$DOTFILES_DIR/cleanup.sh"
@@ -21,26 +23,34 @@ watch_dotfiles() {
   fswatch -0 -r --event Created "$DOTFILES_DIR" | while IFS= read -r -d "" file; do
     rel="${file#"$DOTFILES_DIR"/}"
     target="$HOME/$rel"
-
-    mkdir -p "$(dirname "$target")"
-
+    skip_file=0
     for skip in "${skip_files[@]}"; do
-        if [[ "$file" == "$skip" ]]; then
-            continue 2 
-        fi
+      if [[ "$file" == "$skip" ]]; then
+        skip_file=1
+        break
+      fi
     done
-
-    filename=$(basename "$rel")
-    if [[ $filename =~ ^[0-9]+$ || $file == $DOTFILES_DIR/.git/* ]]; then
+    if [[ $skip_file -eq 1 ]]; then
       continue
     fi
 
+    if [[ "$file" == "$DOTFILES_DIR/.git"* ]]; then
+      continue
+    fi
 
+    filename=$(basename "$rel")
+    if [[ $filename =~ ^[0-9]+$ ]]; then
+      continue
+    fi
+
+    if [[ -d "$file" ]]; then
+      mkdir -p "$target"
+      continue
+    fi
     if [[ ! -e "$target" ]]; then
-      ln -sfn "$file" "$target"
+      mkdir -p "$(dirname "$target")"
+      ln -s "$file" "$target"
       echo "$(date '+%F %T') Linked $file -> $target"
-    else
-      echo "$(date '+%F %T') Skipped $file -> $target. Reason: file already exists"
     fi
 
     sleep 0.2
--- .zshrc
+++ .zshrc
@@ -45,4 +45,5 @@ alias ls='eza --icons=always --group-directories-first --git'
 #SYMLINK_LOGGER="$HOME/dotfiles/.config/zsh/dotfiles-watch.log"
 "$HOME/dotfiles/.config/zsh/dotfiles-symlink-watcher.sh" &
 
+
 fastfetch
