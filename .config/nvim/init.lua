02e4a58ff5daefed2a07d82718f0776999040ff6
bernelius <bob@bernelius.com>
2025-09-26 13:18:23 +0200

    some color picker stuff in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.opt.shiftwidth = tabsize
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
 
 -- WSL clipboard support through win32yank
 if vim.fn.has("wsl") == 1 and vim.fn.executable("win32yank.exe") == 1 then
