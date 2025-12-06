2bddab86fd0182fa110b1afaa67bf4102619c5e3
Nathrak <bob@bernelius.com>
2025-12-06 20:07:45 +0100

    scrolloff, gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,7 @@ vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 3
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.splitright = true
