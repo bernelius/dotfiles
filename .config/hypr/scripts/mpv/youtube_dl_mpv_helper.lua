2d21772c5a3b406ad405734c06f5c4b37607a883
bernelius <bob@bernelius.com>
2026-01-04 12:02:42 +0100

    handle resized windows in the youtube dl mpv helper script

--- .config/hypr/scripts/mpv/youtube_dl_mpv_helper.lua
+++ .config/hypr/scripts/mpv/youtube_dl_mpv_helper.lua
@@ -1,23 +1,38 @@
-local function remove_focus_from_youtube_webapp(event)
-    os.execute("~/.config/hypr/scripts/dispatchers/hypr_next_window_up.sh")
-    os.execute("notify-send 'we playin'")
-    mp.set_property(
-        "title",
-        "YouTubeDL: ${!playlist-count==1:[${playlist-pos-1}/${playlist-count}] }${media-title} - mpv"
-    )
-    os.execute('hyprctl dispatch movetoworkspacesilent special:hidden,class:"^WebApp-YouTube.*"')
-    --in case we spawn in the wrong place
-    --os.execute("hyprctl keyword 'windowrule[monkeytype-workspace-4]:no_focus on'")
-    --os.execute("hyprctl dispatch moveintogroup u")
-    --os.execute("hyprctl keyword 'windowrule[monkeytype-workspace-4]:no_focus off'")
+local function capture_stdout(command)
+    local handle = assert(io.popen(command, "r"))
+    local stdout = assert(handle:read("*a"))
+    handle:close()
+    return stdout
 end
 
-local function grant_focus_to_youtube_webapp(event)
+---@diagnostic disable-next-line: unused-local
+local function hide_youtube_webapp(event)
+    os.execute("~/.config/hypr/scripts/dispatchers/hypr_next_window_up.sh")
+    local current_window_pid = capture_stdout("hyprctl activewindow | grep pid"):gsub("%s+", "")
+
+    os.execute("hyprctl dispatch focuswindow class:^WebApp-YouTube.*")
+    local youtube_window_pid = capture_stdout("hyprctl activewindow | grep pid"):gsub("%s+", "")
+    -- os.execute("notify-send '" .. youtube_window_pid .. "'")
+    mp.add_timeout(0.05, function()
+        os.execute("notify-send 'we playin'")
+        mp.set_property(
+            "title",
+            -- YouTubeDL: + the default title + - mpv
+            "YouTubeDL: ${!playlist-count==1:[${playlist-pos-1}/${playlist-count}] }${media-title} - mpv"
+        )
+        os.execute("hyprctl dispatch movetoworkspacesilent special:hidden," .. youtube_window_pid)
+        if current_window_pid ~= youtube_window_pid then
+            os.execute("hyprctl dispatch focuswindow " .. current_window_pid)
+        end
+    end)
+end
+
+---@diagnostic disable-next-line: unused-local
+local function unhide_youtube_webapp(event)
     os.execute("notify-send 'we quittin'")
     os.execute("~/.config/hypr/scripts/dispatchers/hypr_next_window_up.sh")
     os.execute('hyprctl dispatch movetoworkspacesilent 4,class:"^WebApp-YouTube.*"')
-    --os.execute("hyprctl keyword 'windowrule[youtube-app-workspace-4-opaque]:no_focus off'")
 end
 
-mp.register_event("file-loaded", remove_focus_from_youtube_webapp)
-mp.register_event("end-file", grant_focus_to_youtube_webapp)
+mp.register_event("file-loaded", hide_youtube_webapp)
+mp.register_event("end-file", unhide_youtube_webapp)
