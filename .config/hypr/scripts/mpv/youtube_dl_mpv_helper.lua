10c757d657aec7eb195d21863a6dab0ce5c52c2c
bernelius <bob@bernelius.com>
2026-01-05 15:21:36 +0100

    more robustness in youtube_dl_mpv_helper. change hypridle to never suspend computer.

--- .config/hypr/scripts/mpv/youtube_dl_mpv_helper.lua
+++ .config/hypr/scripts/mpv/youtube_dl_mpv_helper.lua
@@ -1,18 +1,29 @@
 local function capture_stdout(command)
-    local handle = assert(io.popen(command, "r"))
-    local stdout = assert(handle:read("*a"))
+    local handle, err = io.popen(command, "r")
+    if handle == nil then
+        os.execute('notify-send("Failed to run io.popen: "' .. err .. "'")
+        os.exit(1)
+    end
+    local stdout = handle:read("*a")
     handle:close()
     return stdout
 end
 
 ---@diagnostic disable-next-line: unused-local
 local function hide_youtube_webapp(event)
-    os.execute("~/.config/hypr/scripts/dispatchers/hypr_next_window_up.sh")
-    local current_window_pid = capture_stdout("hyprctl activewindow | grep pid"):gsub("%s+", "")
+    os.execute("hyprctl dispatch layoutmsg preselect u")
+    local current_window_pid = capture_stdout("hyprctl activewindow | grep pid | sed 's/[[:space:]]//g'")
 
     os.execute("hyprctl dispatch focuswindow class:^WebApp-YouTube.*")
-    local youtube_window_pid = capture_stdout("hyprctl activewindow | grep pid"):gsub("%s+", "")
-    -- os.execute("notify-send '" .. youtube_window_pid .. "'")
+    -- the -A 5 -B 5 are arbitrary. At the moment the pid is 2 lines below initialClass, but this could change
+    -- after sed is applied the string should be "pid:%d+" without spaces
+    local youtube_window_pid = capture_stdout(
+        'hyprctl clients | grep "initialClass: WebApp-YouTube" -A 8 -B 8 -m 1 | grep pid | sed "s/[[:space:]]//g"'
+    )
+
+    os.execute("hyprctl keyword 'windowrule[youtubedl-workspace-4]:no_focus on'")
+    mp.disable_key_bindings("default")
+    --os.execute("notify-send '" .. youtube_window_pid .. "'")
     mp.add_timeout(0.05, function()
         os.execute("notify-send 'we playin'")
         mp.set_property(
@@ -24,6 +35,10 @@ local function hide_youtube_webapp(event)
         if current_window_pid ~= youtube_window_pid then
             os.execute("hyprctl dispatch focuswindow " .. current_window_pid)
         end
+    end)
+    mp.add_timeout(0.5, function()
+        os.execute("hyprctl keyword 'windowrule[youtubedl-workspace-4]:no_focus off'")
+        mp.enable_key_bindings("default")
     end)
 end
 
